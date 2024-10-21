d39f3d543270ed961afb6b30bed695d0953723b4
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2024-10-22 01:18:06 +0530

    Initial commit

---
+++ src/main/java/com/sandeep/podstream/PodstreamApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.podstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodstreamApplication.class, args);
+	}
+
+}

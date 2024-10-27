ef69bc083d965283e96268c516f1d101006f617e
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2024-10-28 02:03:27 +0530

    sn/dev_oct_w4: Add graphql schema file, user controller, and user service layer

---
+++ src/main/java/com/sandeep/podstream/service/UserService.java
@@ -0,0 +1,21 @@
+package com.sandeep.podstream.service;
+
+import com.sandeep.podstream.model.LoginResponse;
+import com.sandeep.podstream.model.User;
+import com.sandeep.podstream.model.requests.UserRequest;
+import jakarta.transaction.Transactional;
+
+import java.util.UUID;
+
+public interface UserService {
+    User registerUser(UserRequest userRequest);
+
+    @Transactional
+    boolean deleteUserById(UUID userId);
+
+    User getUser(UserRequest userRequest) throws Exception;
+//
+//    boolean updateUser(UserRequest userRequest);
+//
+//    LoginResponse userLogin(UserRequest userRequest);
+}

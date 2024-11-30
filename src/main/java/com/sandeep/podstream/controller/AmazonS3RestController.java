dba51902dcdf7804ade6277431836c1302a4824c
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2024-11-30 08:34:37 +0530

    sn: integrated aws s3

--- src/main/java/com/sandeep/podstream/controller/AmazonS3RestController.java
+++ src/main/java/com/sandeep/podstream/controller/AmazonS3RestController.java
@@ -3,11 +3,9 @@ package com.sandeep.podstream.controller;
 import com.amazonaws.services.s3.model.ListBucketsPaginatedResult;
 import com.sandeep.podstream.service.AwsS3Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.InputStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/s3")
@@ -16,14 +14,8 @@ public class AmazonS3RestController {
     @Autowired
     AwsS3Service awsS3Service;
 
-//    @PostMapping(value = "/upload")
-//    public String uploadFile(@RequestBody MultipartFile multipartFile) throws IOException {
-//        awsS3Service.uploadFile(multipartFile.getName(), multipartFile.getSize(), );
-//    }
-
     @GetMapping("/listBuckets")
-    public ListBucketsPaginatedResult listBucketsPaginatedResult(){
+    public ListBucketsPaginatedResult listBucketsPaginatedResult() {
         return awsS3Service.listBuckets();
     }
 }
-
--- src/main/java/com/sandeep/podstream/service/AwsS3Service.java
+++ src/main/java/com/sandeep/podstream/service/AwsS3Service.java
@@ -2,6 +2,8 @@ package com.sandeep.podstream.service;
 
 import com.amazonaws.Response;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListBucketsPaginatedResult;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,5 +11,13 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public interface AwsS3Service {
+    ListBucketsPaginatedResult listBuckets();
+
+    void uploadFile(String keyName, long contentLength, String contentType, InputStream inputStream) throws IOException;
+
+    S3ObjectInputStream downloadFile(String objectKey);
 }
